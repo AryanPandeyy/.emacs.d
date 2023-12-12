7b8751897a1c2c6580ada998d5c62d5e6d3db0d8
Aryan Pandey <margiaryan@gmail.com>
2023-12-12 20:49:32 +0530

    added treesitter script

---
+++ tree-sitter/batch.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+set -u
+set -e
+
+languages=(
+    'bash'
+    'c'
+    'c-sharp'
+    'clojure'
+    'cmake'
+    'cpp'
+    'css'
+    'scss'
+    'dockerfile'
+    'elisp'
+    'elixir'
+    'glsl'
+    'go'
+    'go-mod'
+    'heex'
+    'html'
+    'janet-simple'
+    'java'
+    'javascript'
+    'json'
+    'julia'
+    'lua'
+    'make'
+    'markdown'
+    'org'
+    'perl'
+    'proto'
+    'python'
+    'ruby'
+    'rust'
+    'scala'
+    'surface'
+    'sql'
+    'toml'
+    'tsx'
+    'typescript'
+    'typst'
+    'verilog'
+    'vhdl'
+    'wgsl'
+    'yaml'
+    'dart'
+    'souffle'
+    'kotlin'
+    'zig'
+    'bison'
+)
+
+if [ -z "${JOBS:-}" ]
+then
+    for language in "${languages[@]}"
+    do
+        ./build.sh "${language}"
+    done
+else
+    printf "%s\n" "${languages[@]}" | xargs -P"${JOBS}" -n1 ./build.sh
+fi
