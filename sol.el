220033f5e75117712475b9fd05c6195c5864a903
Aryan Pandey <margiaryan@gmail.com>
2024-07-23 23:34:27 +0530

    clean

--- sol.el
+++
@@ -1,8 +0,0 @@
-(use-package solidity-mode
-  :straight t)
-(use-package solidity-flycheck
-  :straight t)
-(with-eval-after-load 'eglot
-  (add-to-list 'eglot-server-programs
-               '(solidity-mode . ("nomicfoundation-solidity-language-server" "--stdio"))))
-(provide 'sol)
